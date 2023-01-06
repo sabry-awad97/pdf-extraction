a9fb1d71ec30a91597088d98e8694f2157498a5a
Sabry Awad <dr.sabry1997@gmail.com>
2023-01-07 00:01:50 +0200

    completed requirement 1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import io
 import requests
 from bs4 import BeautifulSoup
 import PyPDF2
+import urllib
 
 
 class PDFScraper:
@@ -25,27 +26,33 @@ class PDFScraper:
 
 
 class PDFSearcher:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
     def search(self, pdf_links, word):
         for link in pdf_links:
+            # Check if the link is a relative or an absolute URL
+            if not urllib.parse.urlparse(link).scheme:
+                # Combine the base URL with the relative URL
+                link = urllib.parse.urljoin(self.base_url, link)
+
             # Make a request to download the PDF file
             response = requests.get(link)
 
             # Open the response content as a binary stream
             stream = io.BytesIO(response.content)
 
-            # Open the PDF file
-            with open(stream, 'rb') as f:
-                # Create a PDF object
-                pdf = PyPDF2.PdfFileReader(f)
+            # Create a PDF object
+            pdf = PyPDF2.PdfFileReader(stream)
 
-                # Iterate over all the pages
-                for page in range(pdf.getNumPages()):
-                    # Extract the text from the page
-                    text = pdf.getPage(page).extractText()
+            # Iterate over all the pages
+            for page in range(pdf.getNumPages()):
+                # Extract the text from the page
+                text = pdf.getPage(page).extractText()
 
-                    # Search for the word in the text
-                    if word in text:
-                        print(f'Found "{word}" in page {page} of {link}')
+                # Search for the word in the text
+                if word in text:
+                    print(f'Found "{word}" in page {page} of {link}')
 
 
 # Test the classes
@@ -53,5 +60,5 @@ url = 'https://www.capitol.hawaii.gov/sessions/session2022/testimony/'
 scraper = PDFScraper(url)
 pdf_links = scraper.scrape_pdf_links()
 
-searcher = PDFSearcher()
+searcher = PDFSearcher("https://www.capitol.hawaii.gov/")
 searcher.search(pdf_links, 'the')
