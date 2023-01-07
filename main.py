dd6342c65eb1f7b43415230c4b5b1e9a5a0022b6
Sabry Awad <dr.sabry1997@gmail.com>
2023-01-07 07:33:47 +0200

    Add error handling

--- main.py
+++ main.py
@@ -10,19 +10,24 @@ class PDFScraper:
         self.url = url
 
     def scrape_pdf_links(self):
-        # Make a request to the webpage
-        response = requests.get(self.url)
+        try:
+            # Make a request to the webpage
+            response = requests.get(self.url)
 
-        # Parse the HTML content
-        soup = BeautifulSoup(response.text, 'html.parser')
+            # Parse the HTML content
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Find all the links to PDF files
-        pdf_links = soup.find_all('a', href=lambda x: x and x.endswith('.pdf'))
+            # Find all the links to PDF files
+            pdf_links = soup.find_all(
+                'a', href=lambda x: x and x.endswith('.pdf'))
 
-        # Extract the PDF links
-        pdf_links = [link['href'] for link in pdf_links]
+            # Extract the PDF links
+            pdf_links = [link['href'] for link in pdf_links]
 
-        return pdf_links
+            return pdf_links
+        except Exception as e:
+            print(f'Error: {e}')
+            return []
 
 
 class PDFSearcher:
@@ -31,28 +36,31 @@ class PDFSearcher:
 
     def search(self, pdf_links, word):
         for link in pdf_links:
-            # Check if the link is a relative or an absolute URL
-            if not urllib.parse.urlparse(link).scheme:
-                # Combine the base URL with the relative URL
-                link = urllib.parse.urljoin(self.base_url, link)
+            try:
+                # Check if the link is a relative or an absolute URL
+                if not urllib.parse.urlparse(link).scheme:
+                    # Combine the base URL with the relative URL
+                    link = urllib.parse.urljoin(self.base_url, link)
 
-            # Make a request to download the PDF file
-            response = requests.get(link)
+                # Make a request to download the PDF file
+                response = requests.get(link)
 
-            # Open the response content as a binary stream
-            stream = io.BytesIO(response.content)
+                # Open the response content as a binary stream
+                stream = io.BytesIO(response.content)
 
-            # Create a PDF object
-            pdf = PyPDF2.PdfFileReader(stream)
+                # Create a PDF object
+                pdf = PyPDF2.PdfFileReader(stream)
 
-            # Iterate over all the pages
-            for page in range(pdf.getNumPages()):
-                # Extract the text from the page
-                text = pdf.getPage(page).extractText()
+                # Iterate over all the pages
+                for page in range(pdf.getNumPages()):
+                    # Extract the text from the page
+                    text = pdf.getPage(page).extractText()
 
-                # Search for the word in the text
-                if word in text:
-                    print(f'Found "{word}" in page {page} of {link}')
+                    # Search for the word in the text
+                    if word in text:
+                        print(f'Found "{word}" in page {page} of {link}')
+            except Exception as e:
+                print(f'Error: {e}')
 
 
 # Test the classes
